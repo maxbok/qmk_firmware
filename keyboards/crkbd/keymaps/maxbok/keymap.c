fa9ecbde1aa13b55851ad2f30232b7893659b301
Maxime Bokobza <max@codehawks.eu>
2023-10-15 21:55:32 +0200

    [crkbd] Add DEL to right shift

--- keyboards/crkbd/keymaps/maxbok/keymap.c
+++ keyboards/crkbd/keymaps/maxbok/keymap.c
@@ -20,7 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SLSH_LM LT(_NAV, KC_SLSH)
 #define CTL_TAB CTL_T(KC_TAB)
+#define SFT_DEL RSFT_T(KC_DEL)
 #define SPC_SYM LT(_SYMBOLS, KC_SPC)
+#define MO_SYM  MO(_SYMBOLS)
 #define TG_UTIL TG(_UTILS)
 
 #define PRV_TAB SCMD(KC_LCBR)
@@ -30,28 +32,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        CTL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  SLSH_LM, KC_RSFT,
-                                            KC_LALT, KC_LCMD, MO(_SYMBOLS),     SPC_SYM, KC_RCMD, KC_RALT
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        CTL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  SLSH_LM, SFT_DEL,
+                                            KC_LALT, KC_LCMD, MO_SYM,    SPC_SYM, KC_RCMD, KC_RALT
     ),
     [_SYMBOLS] = LAYOUT_split_3x6_3(
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-        _______, TG_UTIL, KC_SCLN, KC_COLN, KC_MINS, KC_EQL,                             KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX, _______,
-                                            _______, _______, _______,          _______, _______, _______
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+        _______, TG_UTIL, KC_SCLN, KC_COLN, KC_MINS, KC_EQL,                      KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX, _______,
+                                            _______, _______, _______,   _______, _______, _______
     ),
     [_NAV] = LAYOUT_split_3x6_3(
-        KC_GRV,  KC_BRMD, KC_BRMU, KC_MS_U, KC_VOLD, KC_VOLU,                            XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC_MS_L, KC_BTN1, KC_MS_R, XXXXXXX,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, KC_MS_D, XXXXXXX, XXXXXXX,                            WEB_PRV, PRV_TAB, NXT_TAB, WEB_NXT, XXXXXXX, XXXXXXX,
-                                            _______, _______, _______,          _______, _______, _______
+        KC_GRV,  KC_BRMD, KC_BRMU, KC_MS_U, KC_VOLD, KC_VOLU,                     XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_MS_L, KC_BTN1, KC_MS_R, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, KC_MS_D, XXXXXXX, XXXXXXX,                     WEB_PRV, PRV_TAB, NXT_TAB, WEB_NXT, XXXXXXX, XXXXXXX,
+                                            _______, _______, _______,   _______, _______, _______
     ),
     [_UTILS] = LAYOUT_split_3x6_3(
-        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, TG_UTIL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, TG_UTIL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                            XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 
@@ -66,6 +68,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case SLSH_LM:
             return 150;
         case CTL_TAB:
+            return 140;
+        case SFT_DEL:
             return 140;
         case SPC_SYM:
             return 250;
